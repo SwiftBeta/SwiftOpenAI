7bfa90b3fa2156dda23fde7fc9be11ea676f4e1c
SwiftBeta <swiftbeta.blog@gmail.com>
2023-03-26 18:16:35 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,18 +7,14 @@ let package = Package(
     name: "SwiftOpenAI",
     platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftOpenAI",
             targets: ["SwiftOpenAI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.2.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftOpenAI",
             dependencies: []),
