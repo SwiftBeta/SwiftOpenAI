95c5c27cf605c11add7ea7e1166feaa00f66ea25
SwiftBeta <swiftbeta.blog@gmail.com>
2023-03-26 10:37:15 +0200

    Use latest macOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftOpenAI",
-    platforms: [.iOS(.v16), .macOS(.v12)],
+    platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
