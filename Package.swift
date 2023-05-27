4907a24e0da217a338fa8e778f9a2fb9ebc63927
SwiftBeta <swiftbeta.blog@gmail.com>
2023-05-27 09:51:43 +0200

    Added support iOS 15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftOpenAI",
-    platforms: [.iOS(.v16), .macOS(.v13)],
+    platforms: [.iOS(.v15), .macOS(.v13)],
     products: [
         .library(
             name: "SwiftOpenAI",
